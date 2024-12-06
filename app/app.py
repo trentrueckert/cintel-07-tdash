a0979c18f3f41a6374ec9d9973cbde621208d2b7
TrentRueckert <trentrueckert@gmail.com>
2024-12-05 22:03:58 -0600

    add comments to app.py

--- app/app.py
+++ app/app.py
@@ -1,17 +1,20 @@
+# Import dependencies
 import seaborn as sns
 from faicons import icon_svg
-
 from shiny import reactive
 from shiny.express import input, render, ui
 import palmerpenguins 
 
+# Load the palmerpenguins dataset in a dataframe called df
 df = palmerpenguins.load_penguins()
 
+# Create a title and layout for the page
 ui.page_opts(title="Trent's Perfect Penguins dashboard", bg_color="#0000FF", fillable=True)
 
-
+# Create the sidebar
 with ui.sidebar(title="Filter controls"):
     ui.input_slider("mass", "Mass", 2000, 6000, 6000)
+    # Input a checkbox for species
     ui.input_checkbox_group(
         "species",
         "Species",
@@ -20,6 +23,7 @@ with ui.sidebar(title="Filter controls"):
     )
     ui.hr()
     ui.h6("Links")
+    # Links to different GitHub pages and shiny pages
     ui.a(
         "GitHub Source",
         href="https://github.com/denisecase/cintel-07-tdash",
@@ -47,7 +51,7 @@ with ui.sidebar(title="Filter controls"):
         target="_blank",
     )
 
-
+# Creating a value box for the count for the number of penguins
 with ui.layout_column_wrap(fill=False):
     with ui.value_box(showcase=icon_svg("earlybirds")):
         "Number of penguins"
@@ -56,6 +60,7 @@ with ui.layout_column_wrap(fill=False):
         def count():
             return filtered_df().shape[0]
 
+    # Creating a value box for the average bill length
     with ui.value_box(showcase=icon_svg("ruler-horizontal")):
         "Average bill length"
 
@@ -63,6 +68,7 @@ with ui.layout_column_wrap(fill=False):
         def bill_length():
             return f"{filtered_df()['bill_length_mm'].mean():.1f} mm"
 
+    # Creating a value box for the average bill depth
     with ui.value_box(showcase=icon_svg("ruler-vertical")):
         "Average bill depth"
 
@@ -77,6 +83,7 @@ with ui.layout_columns():
 
         @render.plot
         def length_depth():
+            # Seaborn scatterplot
             return sns.scatterplot(
                 data=filtered_df(),
                 x="bill_length_mm",
@@ -87,6 +94,7 @@ with ui.layout_columns():
     with ui.card(full_screen=True):
         ui.card_header("Penguin Data")
 
+        # DataGrid with summary statistics
         @render.data_frame
         def summary_statistics():
             cols = [
@@ -102,6 +110,7 @@ with ui.layout_columns():
 #ui.include_css(app_dir / "styles.css")
 
 
+# Reactive calc function to filter the data
 @reactive.calc
 def filtered_df():
     filt_df = df[df["species"].isin(input.species())]
